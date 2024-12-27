9aeaaff5856d98a7e74605b8755c32829f175f96
Yuriy Bevov <yuriybevov@gmail.com>
2024-12-27 15:15:25 +0300

    правки шрифтов, убрал ошибки из js

--- src/scripts/entry.js
+++ src/scripts/entry.js
@@ -13,7 +13,7 @@ import './modules/modals';
 import './modules/form-submit';
 import './modules/stuff-card';
 import './modules/tables';
-import './modules/video-container';
+// import './modules/video-container';
 import './modules/gsap/scroll-up-btn';
 
 import { Fancybox } from '@fancyapps/ui';
